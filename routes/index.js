83a52933ce7d8962d4209c14742bad107f607e3f
Stephanie Cray <stephanie.cray@yahoo.com>
2016-12-12 00:04:20 -0500

    added partials, set up routes, and added more view files for the signup, login, and contactform

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index.ejs');
 });
 
+// Get to signup page
+router.get('/signup', function (req,res) {
+  res.render('signup');
+
+});
+
+//Get to login page
+router.get('/login', function (req,res) {
+  res.render('login');
+});
+
+//Get to contact form
+router.get('/contactForm', function (req,res) {
+  res.render('contactForm');
+});
 module.exports = router;
