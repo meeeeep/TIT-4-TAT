ddc6e5ddee101c1d767ed99ed3a908eb4a99ec7b
Stephanie Cray <stephanie.cray@yahoo.com>
2016-12-15 12:45:30 -0500

    updated database code.

--- app.js
+++ app.js
@@ -20,7 +20,20 @@ var contacts = require('./routes/contacts');
 
 
 var app = express();
-mongoose.connect('mongodb://localhost/project2');
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect('mongodb://localhost/project-2');
+}
+mongoose.connection.on('error', function(err) {
+        console.error('MongoDB connection error: ' + err);
+        process.exit(-1);
+    }
+);
+mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+});
 
 
 // view engine setup
@@ -94,6 +107,8 @@ app.use(function(err, req, res, next) {
 });
 
 console.log('Running in %s mode', app.get('env'));
+
+
 
 
 module.exports = app;
